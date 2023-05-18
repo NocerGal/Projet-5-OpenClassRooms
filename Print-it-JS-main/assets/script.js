20e9dffabe68431bca3a8d226150b0ce2b8929a7
Luc Schenherr <luc.schenherr@hotmail.fr>
2023-05-18 17:05:33 +0200

    Correction de la fonction pour les eventsListener

--- Print-it-JS-main/assets/script.js
+++ Print-it-JS-main/assets/script.js
@@ -25,63 +25,12 @@ const selectBanner = document.getElementById("banner");
 // Permet de sélectionner la flèche droite
 const rightArrow = selectBanner.lastElementChild;
 const leftArrow = selectBanner.firstElementChild;
+let indexSlides = slides.length;
 let currentSlide = 0;
-let indexSlides = slides.length - 1;
 const dotsSection = document.querySelector(".dots");
 const selectBannerImage = document.querySelector(".banner-img");
 
-// renomemer slideFocus en Slide Index ou currentSlide
-// function nom(a, b){
-// return a.prix-b.prix;
-// }
-
-// nom(slides[0], slides[1]);
-
-// console.log(nom(slides[0], slides[1]));
-
-function moveCarousselLeft() {
-	if (currentSlide == 0) {
-		dotFocus[currentSlide].classList.remove("dot_selected");
-		currentSlide = currentSlide + indexSlides;
-		dotFocus[currentSlide].classList.add("dot_selected");
-		selectBannerImage.src = "./assets/images/slideshow/slide" + (currentSlide + 1) + ".png";
-	} else {
-		dotFocus[currentSlide].classList.remove("dot_selected");
-		currentSlide--;
-		dotFocus[currentSlide].classList.add("dot_selected");
-		selectBannerImage.src = "./assets/images/slideshow/slide" + (currentSlide + 1) + ".jpg";
-		console.log(currentSlide)
-	}
-	// console.log(slideFocus)
-}
-
-leftArrow.addEventListener("click", moveCarousselLeft);
-
-rightArrow.addEventListener("click", function () {
-	// console.log(slideFocus)
-	if (currentSlide == indexSlides) {
-		dotFocus[currentSlide].classList.remove("dot_selected");
-		currentSlide = currentSlide - indexSlides;
-		dotFocus[currentSlide].classList.add("dot_selected");
-		selectBannerImage.src = "./assets/images/slideshow/slide" + (currentSlide + 1) + ".jpg";
-		console.log(currentSlide)
-	} else {
-		dotFocus[currentSlide].classList.remove("dot_selected");
-		currentSlide = currentSlide + 1;
-		// console.log(slideFocus)
-		dotFocus[currentSlide].classList.add("dot_selected");
-		if (currentSlide == indexSlides) {
-			selectBannerImage.src = "./assets/images/slideshow/slide4.png";
-			console.log(currentSlide);
-		} else {
-			selectBannerImage.src = "./assets/images/slideshow/slide" + (currentSlide + 1) + ".jpg";
-		};
-	}
-});
-
-// AJout des dots
 for (let i = 0; i < slides.length; i++) {
-
 	// Va créer des div pour chaque dot
 	const dotsAdd = document.createElement("div");
 	// Va attribuer la class .dot à chaque div
@@ -90,7 +39,20 @@ for (let i = 0; i < slides.length; i++) {
 	dotsSection.appendChild(dotsAdd);
 }
 
+let moveCaroussel = (x) => 
+{
+	dotFocus[currentSlide].classList.remove("dot_selected");
+	currentSlide = ((currentSlide+indexSlides+x) % indexSlides);
+	dotFocus[currentSlide].classList.add("dot_selected");
+	selectBannerImage.src = "./assets/images/slideshow/"+ slides[currentSlide].image;
+}
+
+// AJout des dots
+
 // Permet d'attribuer un rond plein au premier dot au chargement de la page. Cette partie doit se mettre après la boucle permettant de créer les dots. Je voulais initiallement mettre ce code après la déclaration des let et const mais à ce moment les dots ne sont pas créés donc ne fonctionne pas.
 const dotFocus = document.querySelectorAll(".dot");
 
 dotFocus[0].classList.add("dot_selected");
+
+leftArrow.addEventListener("click", moveCaroussel(1));
+rightArrow.addEventListener("click", moveCaroussel(-1))
